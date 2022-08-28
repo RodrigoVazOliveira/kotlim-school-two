a3f5dcdf0a3fb8c0e8aff58bee959330aa638162
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-08-28 18:55:17 -0300

    usando first apra procurar o topico por id

--- src/main/kotlin/br/dev/rvz/forum/controllers/TopicRestController.kt
+++ src/main/kotlin/br/dev/rvz/forum/controllers/TopicRestController.kt
@@ -3,6 +3,7 @@ package br.dev.rvz.forum.controllers
 import br.dev.rvz.forum.models.Topic
 import br.dev.rvz.forum.services.TopicService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,5 +14,10 @@ class TopicRestController(val topicService: TopicService) {
     @GetMapping
     fun list(): List<Topic> {
         return topicService.list()
+    }
+
+    @GetMapping("/{id}")
+    fun getTopicById(@PathVariable id: Long): Topic {
+        return topicService.getTopicById(id)
     }
 }
--- src/main/kotlin/br/dev/rvz/forum/services/TopicService.kt
+++ src/main/kotlin/br/dev/rvz/forum/services/TopicService.kt
@@ -6,12 +6,11 @@ import br.dev.rvz.forum.models.Topic
 import br.dev.rvz.forum.models.User
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
-import java.util.*
 
 @Service
-class TopicService {
+class TopicService(private var topics: List<Topic>) {
 
-    fun list() : List<Topic> {
+    init {
         val user = User(id = 1, name = "Rodrigo Vaz", email = "rodrigo@gmail.com")
         val course = Course(id = 1, name = "Spring Boot", "Programacao")
         val topic = Topic(
@@ -25,6 +24,36 @@ class TopicService {
             status = StatusTopic.NO_RESPONSE
         )
 
-        return Arrays.asList(topic, topic, topic)
+        val topicTwo = Topic(
+            id = 2,
+            title = "Duvida em Java",
+            message = "Estou com problema em java",
+            dateTimeCreated = LocalDateTime.now(),
+            course = course,
+            author = user,
+            responses = listOf(),
+            status = StatusTopic.NO_RESPONSE
+        )
+
+        val topicThree = Topic(
+            id = 3,
+            title = "Duvida em Java",
+            message = "Estou com problema em java",
+            dateTimeCreated = LocalDateTime.now(),
+            course = course,
+            author = user,
+            responses = listOf(),
+            status = StatusTopic.NO_RESPONSE
+        )
+
+        topics = arrayListOf(topic, topicTwo, topicThree)
+    }
+
+    fun list(): List<Topic> {
+        return topics
+    }
+
+    fun getTopicById(id: Long): Topic {
+        return topics.first { topic -> topic.id == id }
     }
 }
