73c04cee170a272bc8e48c86157db01d0d295589
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-08-30 23:26:15 -0300

    Obtendo a validacao de entrada

--- src/main/kotlin/br/dev/rvz/forum/exceptions/ExceptionHandler.kt
+++ src/main/kotlin/br/dev/rvz/forum/exceptions/ExceptionHandler.kt
@@ -1,7 +1,9 @@
 package br.dev.rvz.forum.exceptions
 
 import br.dev.rvz.forum.models.dto.ErrorResponseDTO
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -17,6 +19,39 @@ class ExceptionHandler {
             message = e.message,
             status = HttpStatus.NOT_FOUND.value(),
             error = HttpStatus.NOT_FOUND.name,
+            path = httpRequest.servletPath
+        )
+    }
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun exception(e: Exception, httpRequest: HttpServletRequest): ErrorResponseDTO {
+        return ErrorResponseDTO(
+            message = e.message,
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            error = HttpStatus.INTERNAL_SERVER_ERROR.name,
+            path = httpRequest.servletPath
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun notFoundException(e: MethodArgumentNotValidException, httpRequest: HttpServletRequest): ErrorResponseDTO {
+        val errorMessage = HashMap<String, String?>()
+        e.bindingResult.fieldErrors.forEach { fieldError ->
+            errorMessage.put(
+                fieldError.field,
+                fieldError.defaultMessage
+            )
+        }
+        val objectMapper = jacksonObjectMapper()
+        val errorMessageToJson = objectMapper.writeValueAsString(errorMessage)
+
+
+        return ErrorResponseDTO(
+            message = errorMessageToJson,
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
             path = httpRequest.servletPath
         )
     }
--- src/main/kotlin/br/dev/rvz/forum/models/dto/topics/TopicRequestDTO.kt
+++ src/main/kotlin/br/dev/rvz/forum/models/dto/topics/TopicRequestDTO.kt
@@ -5,8 +5,8 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 class TopicRequestDTO(
-    @field:NotEmpty
-    @field:Size(min = 5, max = 100)
+    @field:NotEmpty(message = "O campo titulo esta vazio")
+    @field:Size(min = 5, max = 100, message = "O texto deve ser maior que 5 e no maximo 100 caracteres")
     val title: String,
     @field:NotEmpty
     val message: String,
