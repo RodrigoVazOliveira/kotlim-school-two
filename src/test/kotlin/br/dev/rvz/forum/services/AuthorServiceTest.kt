7c6c3a064fd5f8d210d1515c0223b7b19e925ffb
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-09-06 05:13:00 -0300

    Adicionar novo teste unitario do autor

--- src/test/kotlin/br/dev/rvz/forum/services/AuthorServiceTest.kt
+++ src/test/kotlin/br/dev/rvz/forum/services/AuthorServiceTest.kt
@@ -1,5 +1,53 @@
 package br.dev.rvz.forum.services
 
-internal class AuthorServiceTest() {
+import br.dev.rvz.forum.mappers.authors.AuthorModelRequestMapper
+import br.dev.rvz.forum.models.User
+import br.dev.rvz.forum.models.dto.AuthorSavedDTO
+import br.dev.rvz.forum.repositories.AuthorRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
 
+internal class AuthorServiceTest {
+
+
+    private val authorRepository: AuthorRepository = mockk()
+    private val authorModelRequestMapper: AuthorModelRequestMapper = mockk()
+
+    val authorService: AuthorService = AuthorService(
+        authorRepository, authorModelRequestMapper
+    )
+
+    @Test
+    fun `dev salvar um author no banco`() {
+        val userSaved: User = User(
+            id = 1,
+            name = "Jorge",
+            email = "jorge@gmail.com",
+            password = "12345",
+            role = listOf()
+        )
+
+        val user = User(
+            id = null,
+            name = "Jorge",
+            email = "jorge@gmail.com",
+            password = "12345",
+            role = listOf()
+        )
+
+        every { authorRepository.save(any()) } returns userSaved
+        every { authorModelRequestMapper.map(any()) } returns user
+        val actual = authorService.save(
+            AuthorSavedDTO(
+                name = "Jorge",
+                email = "jorge@gmail.com",
+                password = "123456"
+            )
+        )
+
+        verify(exactly = 1) { authorRepository.save(any()) }
+        verify(exactly = 1) { authorModelRequestMapper.map(any()) }
+    }
 }
